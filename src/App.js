14d1a108e2691d111527e17150d65c2cdcaee613
dipannita5 <dipannita.roy@travclan.com>
2024-06-05 10:00:23 +0530

    Charts data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import TimeSeries from './timeSeries'
 
 function App() {
+  const [data, setData] = useState(
+    Array.from({ length: 50 }, () => Math.round(Math.random() * 100))
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        Time series chart
+        <TimeSeries data={data} />
+      </div>
     </div>
   );
 }
